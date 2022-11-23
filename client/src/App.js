ed79ef50704a0541e738249760fe23e444cd5af8
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-11-23 17:04:48 +0100

    AllFixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
-import FunnyWish from './Components/FunnyWish';
-import Sound from './Components/Sound';
-import Timer from './Components/Timer';
-import Names from './Components/Names';
+import FunnyWish from './Componets/FunnyWish';
+import Sound from './Componets/Sound';
+import Timer from './Componets/Timer';
+import Names from './Componets/Names';
+// import FunnyWish from '/Components/FunnyWish';
+// import Sound from '/Components/Sound';
+// import Timer from '/Components/Timer';
+// import Names from './Components/Names';
 
 function App() {
   
