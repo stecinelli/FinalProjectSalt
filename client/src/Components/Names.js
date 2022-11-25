1d2bba1a2082d44532d110d1d08d464b31fb477d
Dariusz-Wolontariusz <109731750+Dariusz-Wolontariusz@users.noreply.github.com>
2022-11-25 10:31:23 +0100

    For demo

--- client/src/Components/Names.js
+++ client/src/Components/Names.js
@@ -8,7 +8,7 @@ const LOCAL_STORAGE_KEY = 'final_project';
 
 const Names = () => {
   const [names, setNames] = useState([])
-  const [isActive, setIsActive] = useState(false)
+  // const [isActive, setIsActive] = useState(false)
 
   useEffect(() => {
     const nameStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -23,26 +23,34 @@ const Names = () => {
 
   const addName = name => {
     const id = uuidv4()
-    setNames([...names, {id, name, isActive}])
+    setNames([...names, {id, name, isActive: false}])
   }
-  console.log(isActive)
+  // console.log(names)
 
   const deleteName = id => {
+    console.log('dariusz wolontariusz');
     setNames(names.filter(person => person.id !== id))
+  }
+
+  // toggle activator zrob to if statement
+  const toggleActivator = (id) => {
+    // setNames(names.map(person => (person.id === id) ? {...person, isActive: !person.isActive} : person))
+    setNames(names.map(person => (person.id !== id) ? {...person, isActive: false} : {...person, isActive: true}))
+    // setNames(names.map(person => (person.id === id) ? {...person, isActive: true} : person))
   }
 
 
   return (
     <div className='name-list'>
+      <Input 
+        names={names}
+        addName={addName}/>
       <Cards
-        setIsActive={setIsActive}
+        toggleActivator={toggleActivator}
         setNames={setNames}
         names={names}
         deleteName={deleteName}
         />
-        <Input 
-        names={names}
-        addName={addName}/>
     </div>
   )
 }
