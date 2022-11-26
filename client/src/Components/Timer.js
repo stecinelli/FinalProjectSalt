be5b2fd1a9826bd9bae4438d39851512ab30837f
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-11-26 10:34:39 +0100

    friday

--- client/src/Components/Timer.js
+++ client/src/Components/Timer.js
@@ -8,7 +8,7 @@ import { BiCaretDown, BiCaretUp } from "react-icons/bi";
 
 const Timer = () => {
     //setting initial counter time and transition time
-    const initialCounter = 6
+    const initialCounter = 10
     const transitionTime = 5
     //ref ensures there's only one interval set - you need to assign the interval id to ref to keep track of it
     const Ref = useRef(null);
@@ -16,23 +16,23 @@ const Timer = () => {
     const [playing, setPlaying] = useState(false)
     const [counter, setCounter] = useState(initialCounter);
     const [isChanging, setIsChanging] = useState(false)
-    
+
     //  checking [counter] every time it changes. handling transition.
     useEffect(() => {
-if (counter === -1) {
-    if (isChanging) {
-        setCounter(initialCounter)
-        setIsChanging(false)
-        document.getElementById('player').pause()
-    }
-    else {
-        setCounter(transitionTime)
-        setIsChanging (true)
-        document.getElementById('player').play()
-    }
-}
-      }, [counter]);
-// just making the timer
+        if (counter === -1) {
+            if (isChanging) {
+                setCounter(initialCounter)
+                setIsChanging(false)
+                document.getElementById('player').pause()
+            }
+            else {
+                setCounter(transitionTime)
+                setIsChanging(true)
+                document.getElementById('player').play()
+            }
+        }
+    }, [counter]);
+    // just making the timer
     const formatTimer = (count) => {
         const mins = Math.floor(count / 60)
         const seconds = count % 60;
@@ -43,13 +43,13 @@ if (counter === -1) {
     }
 
     const startTimer = () => {
-//clearing the interval - ref holds the id of the interval. if we didnt have this it would speed up every time we clicked start
+        //clearing the interval - ref holds the id of the interval. if we didnt have this it would speed up every time we clicked start
         if (Ref.current) clearInterval(Ref.current);
-// calling a function that reduces out counter by one every second. setting minimum of -1 (we had zero before but didn't work)
+        // calling a function that reduces out counter by one every second. setting minimum of -1 (we had zero before but didn't work)
         const id = setInterval(() => {
             setCounter(count => Math.max(-1, count - 1))
         }, 1000)
-// this is useful for pausing, clearing and starting timer. it will allow us to call clear interval function
+        // this is useful for pausing, clearing and starting timer. it will allow us to call clear interval function
         Ref.current = id;
         setPlaying(true)
     }
@@ -64,45 +64,54 @@ if (counter === -1) {
         pauseTimer()
         setCounter(initialCounter)
     }
-//my button alternates between stop and start based on state.
-// for now all we do with playing is set the button
+    //my button alternates between stop and start based on state.
+    // for now all we do with playing is set the button
     let button;
     if (playing) {
         button = <button onClick={pauseTimer}>
-          <img src={pauseButton} alt='pause'/>
+            <img src={pauseButton} alt='pause' />
         </button>;
 
     } else {
         button = <button onClick={startTimer}>
-          <img src={playButton} alt='play'/>
+            <img src={playButton} alt='play' />
         </button>;
     }
     const increaseCounterMinutes = () => {
-        setCounter(count => count+60)
+        setCounter(count => count + 60)
     }
-    const decreaseCounterMinutes = () => { 
+    const decreaseCounterMinutes = () => {
         if (counter >= 60) {
-        setCounter(count => count-60)}
+            setCounter(count => count - 60)
+        }
     }
-    const decreaseCounterSeconds = () => { 
+    const decreaseCounterSeconds = () => {
         if (counter >= 1) {
-        setCounter(count => count-1)}
+            setCounter(count => count - 1)
+        }
     }
     const increaseCounterSeconds = () => {
-        setCounter(count => count+1)
+        setCounter(count => count + 1)
     }
 
     return (
         <div className='Timer'>
-            <BiCaretUp className='minutesUpButton' onClick={increaseCounterMinutes}/> 
-            <BiCaretUp className='secondsUpButton' onClick={increaseCounterSeconds}/> 
+            <div className='top-of-clock'>
+                <BiCaretUp className='minutesUpButton change-time-button' onClick={increaseCounterMinutes} />
+                <BiCaretUp className='secondsUpButton change-time-button' onClick={increaseCounterSeconds} />
+            </div>
+            <div className='clock-view'>{formatTimer(counter)}</div>
+            <div className='bottom-of-clock'>
+                <BiCaretDown className='minutesDownButton change-time-button' onClick={decreaseCounterMinutes} />
+                <BiCaretDown className='secondsDownButton change-time-button' onClick={decreaseCounterSeconds} />
+            </div>
 
-            <div>{formatTimer(counter)}</div>
-            <BiCaretDown className='minutesDownButton' onClick={decreaseCounterMinutes}/> 
-            <BiCaretDown className='secondsDownButton'onClick={decreaseCounterSeconds}/> 
             <br></br>
-            <button onClick={stopTimer}><img src={stopButton} alt='stop'/></button>
-            {button}
+            <div className='two-buttons'>
+                <button onClick={stopTimer}><img src={stopButton} alt='stop' /></button>
+                {button}
+            </div>
+
         </div>
     )
 }
