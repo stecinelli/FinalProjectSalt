08716546f044811b60f76c05bf68d5c147da1842
Paula Zappi <paula.zappi@appliedtechnology.se>
2022-11-29 09:27:07 +0100

    circle

--- client/src/Components/Timer.js
+++ client/src/Components/Timer.js
@@ -3,10 +3,10 @@ import playButton from './Buttons/play-button-black.png'
 import stopButton from './Buttons/stop-button-black.png'
 import pauseButton from './Buttons/pause-button-black.png'
 import { BiCaretDown, BiCaretUp } from "react-icons/bi";
+import "./Timer.css"
 
 
-
-const Timer = () => {
+const Timer = (props) => {
     //setting initial counter time and transition time
     const initialCounter = 6
     const transitionTime = 5
@@ -16,13 +16,17 @@ const Timer = () => {
     const [playing, setPlaying] = useState(false)
     const [counter, setCounter] = useState(initialCounter);
     const [isChanging, setIsChanging] = useState(false)
+    //time we edited
     const [selectedTime, setSelectedTime] = useState(initialCounter)
     const [minutes, setMinutes] = useState('')
     const [seconds, setSeconds] = useState('')
     const [autonext, setAutonext] = useState(true)
     const [timeModified, setTimeModified] = useState(false)
-
-
+    // calculating the percentage of the circle
+    const circleStyle = {
+        //if time is being modified we set it to zero. otherwise we workout what percentage of time we have gone through
+        '--percent': timeModified ? 0 : (selectedTime - counter) / selectedTime * 100,
+    };
 
 
     //  checking [counter] every time it changes. handling transition.
@@ -31,27 +35,30 @@ const Timer = () => {
             if (isChanging) {
                 setCounter(selectedTime)
                 setIsChanging(false)
-                if(!autonext) {
+                if (!autonext) {
                     pauseTimer()
                 }
+
                 // document.getElementById('player').pause()
             }
             else {
                 setCounter(transitionTime)
                 setIsChanging(true)
-                // document.getElementById('player').play()
+
+                // We have finished timer, time to change person and beep etc
+                // props.timeUp()
             }
         }
-
-
+        // 
+        //this is formatting the timer by calculating mins and secs from counter
         let mins = Math.floor(counter / 60)
         let secs = counter % 60
-        if (mins <= 9) {mins = '0' + Number(mins)}
-        if (secs <= 9) {secs = '0' + Number(secs)}
+        if (mins <= 9) { mins = '0' + Number(mins) }
+        if (secs <= 9) { secs = '0' + Number(secs) }
         setMinutes(mins)
         setSeconds(secs)
     }, [counter]);
-
+    //getting whatever they typed and setting it as new time, setTimeModified- when we hit play we want to know if we interacted with the time so we set a new time
     const editTimerMinutes = (newMinutes) => {
         setMinutes(newMinutes)
         setTimeModified(true)
@@ -63,12 +70,12 @@ const Timer = () => {
 
         // setCounter(60 * minutes + seconds)
     }
-
+    //enforcing the rules of input - whenever we click away from input box it will format it, reducing numbers over 59 to 59. enforcing rules of time
     const adjustTime = () => {
-        if(seconds > 59) {
+        if (seconds > 59) {
             setSeconds(59)
         }
-        if(minutes > 59) {
+        if (minutes > 59) {
             setMinutes(59)
         }
         if (minutes <= 9) setMinutes('0' + Number(minutes))
@@ -76,20 +83,21 @@ const Timer = () => {
 
 
     }
-
+    //toggle between true and false to see if we autostart or not when we hit zero
     const toggleAuto = () => {
         setAutonext(current => !current)
     }
 
     // just making the timer
     const timer = () => {
-
+        //disable input while playing -
         if (playing) {
-            return <> <input className='inputTimer' type="text" value={minutes} onBlur={e => adjustTime()} onChange={e => editTimerMinutes(e.target.value)} disabled
-            /> : <input className='inputTimer' type="text" value={seconds} onBlur={e => adjustTime()} onChange={e => editTimerSeconds(e.target.value)} disabled /> </>
+            return <> <input className='inputTimer' type="text" value={minutes} disabled
+            /> : <input className='inputTimer' type="text" value={seconds} disabled /> </>
         }
+        //this is able to be modified since it's not playing -  onBlur is when we aren't clicked inside input anymore. we call function to adjust time - max 59. onChange- every time it's modified we are editing the state
         return <> <input className='inputTimer' type="text" value={minutes} onBlur={e => adjustTime()} onChange={e => editTimerMinutes(e.target.value)}
-        /> : <input className='inputTimer'  type="text" value={seconds} onBlur={e => adjustTime()} onChange={e => editTimerSeconds(e.target.value)} /> </>
+        /> : <input className='inputTimer' type="text" value={seconds} onBlur={e => adjustTime()} onChange={e => editTimerSeconds(e.target.value)} /> </>
 
 
 
@@ -98,14 +106,19 @@ const Timer = () => {
     }
 
     const startTimer = () => {
-        
-        const newTime = 60 * Number(minutes) + Number(seconds)
-        if(timeModified) {
+        //we see where the current mins and secs are and calculate new counter value. 
+
+        if (timeModified) {
+            //calculating number of seconds
+            const newTime = 60 * Number(minutes) + Number(seconds)
+            //setting counter
             setCounter(newTime)
+            //when we start the clock again we will see new time, we also need this for the circle
             setSelectedTime(newTime)
+            //then it's no longer modified - we only want true after we edited it otherwise when we press pause and play again it would reset the newTime
             setTimeModified(false)
         }
-      
+
 
         //clearing the interval - ref holds the id of the interval. if we didnt have this it would speed up every time we clicked start
         if (Ref.current) clearInterval(Ref.current);
@@ -142,6 +155,7 @@ const Timer = () => {
         </button>;
     }
     //set the new initial counter with the number that has been selected, setting new counter and not letting it change while timer is running
+    //these are the functions being called when I click on arrows
     const increaseCounterMinutes = () => {
         if (!playing) {
             setCounter(count => count + 60)
@@ -154,7 +168,7 @@ const Timer = () => {
         if (!playing) {
             if (counter >= 60) {
                 setCounter(count => count - 60)
-                
+
 
             }
             setSelectedTime(counter)
@@ -181,21 +195,38 @@ const Timer = () => {
     }
 
     return (
-        <div className='Timer'>
-           <div className='buttons__up__down'> <BiCaretUp className='minutesUpButton' onClick={increaseCounterMinutes} />
-            <BiCaretUp className='secondsUpButton' onClick={increaseCounterSeconds} />
+        <div className="box">
+            <div className="card">
+                <div className="percent">
+                    {/* circle starts */}
+                    <svg className="svgCircle">
+                        <circle cx="105" cy="105" r="100"></circle>
+                        <circle cx="105" cy="105" r="100" style={circleStyle}></circle>
+                        {/* circle ends */}
+                    </svg>
+                    <div className="number">
 
-            {timer()}
-            <BiCaretDown className='minutesDownButton' onClick={decreaseCounterMinutes} />
-            <BiCaretDown className='secondsDownButton' onClick={decreaseCounterSeconds} />
+                        <div className='Timer'>
+                            <div className='buttons__up__down'>
+                                <BiCaretUp className='minutesUpButton' onClick={increaseCounterMinutes} />
+                                <BiCaretUp className='secondsUpButton' onClick={increaseCounterSeconds} />
+
+                                {timer()}
+                                <BiCaretDown className='minutesDownButton' onClick={decreaseCounterMinutes} />
+                                <BiCaretDown className='secondsDownButton' onClick={decreaseCounterSeconds} />
+                            </div>
+                            <br></br>
+                            <button onClick={stopTimer}><img src={stopButton} alt='stop' /></button>
+
+                            {button}
+                            <button onClick={toggleAuto}>Toggle auto: {autonext ? "ON" : "OFF"}</button>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-            <br></br>
-            <button onClick={stopTimer}><img src={stopButton} alt='stop' /></button>
-
-            {button}
-            <button onClick={toggleAuto}>Toggle auto: {autonext? "ON":"OFF"}</button>
-
         </div>
+
     )
 }
 
